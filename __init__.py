674bea566e267008dc079cbd79e18bab6473e849
figowitz1 <68816001+figowitz1@users.noreply.github.com>
2025-06-07 10:36:19 -0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,27 +1,8 @@
-# This file is part of the party_ar module for Tryton.
-# The COPYRIGHT file at the top level of this repository contains
-# the full copyright notices and license terms.
-
 from trytond.pool import Pool
-from . import company
 from . import party
-from . import address
-from . import afip
-
 
 def register():
     Pool.register(
-        afip.PyAfipWsWrapper,
-        afip.AFIPCountry,
-        company.Company,
-        party.Configuration,
-        party.AFIPVatCountry,
         party.Party,
-        party.PartyIdentifier,
-        party.GetAFIPDataStart,
-        party.Cron,
-        address.Address,
-        module='party_ar', type_='model')
-    Pool.register(
-        party.GetAFIPData,
-        module='party_ar', type_='wizard')
+        module='party_co', type_='model'
+    )
